d5fd80f09b57316eae7a3795482f95e13b590497
ALifeLivedFully <alifelivedfully.personal@gmail.com>
2019-06-05 15:15:08 -0500

    learned a bit about arrays and slices

---
+++ Tutorials/Traversy/arrays/arrays.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+
+func main()  {
+	//var pronoun [] string
+	//var honorific [] string
+	name := [3] string {"Dani", "m", "Morrison"} //this is an array
+	tags := [] string {"these", "are", "random", "tags"}
+	// name[0] = "Dani"
+	// name[1] = "m"
+	// name[2] = "Morrison"
+
+	fmt.Println(name)
+	fmt.Println(tags)
+	fmt.Println(tags[2])
+}
